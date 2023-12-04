bad8120d6dc25f9990b62a910530c8a256e5430e
Murad khan <murad.jaddoon@gmail.com>
2023-12-05 02:10:38 +0500

    added calender

---
+++ src/pages/Calender/event-utils.tsx
@@ -0,0 +1,23 @@
+import { EventInput } from '@fullcalendar/core'
+
+let eventGuid = 0
+const todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+const endStr = new Date("2023-12-05").toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: todayStr,
+    end: endStr
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: todayStr + 'T12:00:00'
+  }
+]
+
+export function createEventId() {
+  return String(eventGuid++)
+}
