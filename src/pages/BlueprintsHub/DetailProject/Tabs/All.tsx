b65682f4d9ea699171ea2150f3ad3eaa53a0a498
AhmedHassan588 <syedahmedhassan588@gmail.com>
2023-12-18 21:27:32 +0500

    Add detail tabs

---
+++ src/pages/BlueprintsHub/DetailProject/Tabs/All.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useMemo, useState } from 'react'
+import { BlueProjectcolumns } from '~/columns/FormsColumns'
+import { DropDown, DynamicTable, SearchField } from '~/components'
+import { getMockBlueForms } from '~/mocks'
+import { itemsActions } from '~/utils/options'
+
+const All = () => {
+  const [data, setData] = useState<Partial<IBlueForm>[] | []>([])
+  const [selectedRowKeys, setSelectedRowKeys] = useState<any>([])
+  const [loadingData, setLoadingData] = useState<boolean>(false)
+  const [search, setSearch] = useState<string>('')
+  const [pagination, setPagination] = useState<IPagination>({
+    current: 1,
+    pageSize: 7,
+    total: 0
+  })
+
+  useEffect(() => {
+    setLoadingData(true)
+    const fetchData = getMockBlueForms(20, true)
+    setData(fetchData)
+    setLoadingData(false)
+  }, [pagination])
+
+  const handlePaginationChange = (pg: IPagination): void => {
+    setPagination((prevPagination) => ({
+      ...prevPagination,
+      current: pg.current,
+      pageSize: pg.pageSize
+    }))
+  }
+
+  const handleClickItem = (key: string) => {
+    console.log('handleClickItem key: ', key)
+  }
+
+  const handleResolve = (id: number | string) => {
+    console.log('resolved', id)
+  }
+
+  const handleDelete = (id: number | string) => {
+    console.log('deleted', id)
+  }
+
+  const onSelectChange = (newSelectedRowKeys: any) => {
+    console.log('selectedRowKeys changed: ', newSelectedRowKeys)
+    setSelectedRowKeys(newSelectedRowKeys)
+  }
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange
+  }
+  return (
+    <div className='table-container'>
+      <div className='actions-wrapper'>
+        <div className='search-box'>
+          <SearchField
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Search...'
+          />
+        </div>
+        <DropDown items={itemsActions} handleClickItem={handleClickItem} />
+      </div>
+      {useMemo(() => {
+        return (
+          <DynamicTable
+            dataSource={data}
+            columns={BlueProjectcolumns(handleResolve, handleDelete)}
+            isLoading={loadingData}
+            pagination={pagination}
+            handlePaginationChange={handlePaginationChange}
+            rowSelection={rowSelection}
+          />
+        )
+      }, [data, selectedRowKeys])}
+    </div>
+  )
+}
+
+export default All
