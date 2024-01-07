e2ea062977a93666b6a40516cf76506401fbe12e
Murad khan <murad.jaddoon@gmail.com>
2024-01-08 01:47:24 +0500

    Update _utils.ts

--- src/store/services/_utils.ts
+++ src/store/services/_utils.ts
@@ -1,7 +1,9 @@
-export const baseUrl =
-  import.meta.env.VITE_APPLICATION_STATUS === 'development'
-    ? import.meta.env.VITE_DEVELOPMENT_BASE_URL
-    : import.meta.env.VITE_PRODUCTION_BASE_URL
+// export const baseUrl =
+//   import.meta.env.VITE_APPLICATION_STATUS === 'development'
+//     ? import.meta.env.VITE_DEVELOPMENT_BASE_URL
+//     : import.meta.env.VITE_PRODUCTION_BASE_URL
+
+export const baseUrl = 'http://54.208.249.184/api/v1'
 
 export const prepareHeaders = (headers: Headers) => {
   const token = localStorage.getItem('token') ?? ''
