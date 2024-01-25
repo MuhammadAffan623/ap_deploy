31c137786ea145686e68f91cc254fa6a6860a172
Murad khan <murad.jaddoon@gmail.com>
2024-01-25 23:51:43 +0500

    removed extra loader

--- src/pages/UserAndGroups/index.tsx
+++ src/pages/UserAndGroups/index.tsx
@@ -3,14 +3,13 @@ import { CSSProperties, useEffect } from 'react'
 import { Card, ImagesBox, UserGroupCard } from '~/components'
 import NewGroupImage from '~/assets/images/new-group.png'
 import { useGroupsSelector } from '~/store/hooks'
-import { Loader } from '~/components'
 import { useNavigate } from 'react-router-dom'
 import UserTable from './UserTable'
 import './style.scss'
 import { useGetGroupsMutation } from '~/store/services/groups.service'
 
 const UserAndGroups = () => {
-  const [fetchGroups, { isLoading }] = useGetGroupsMutation()
+  const [fetchGroups] = useGetGroupsMutation()
   const { groups } = useGroupsSelector()
 
   const navigate = useNavigate()
@@ -35,46 +34,40 @@ const UserAndGroups = () => {
   return (
     <>
       <Row gutter={[30, 30]}>
-        {isLoading ? (
-          <Loader fullScreen />
-        ) : (
-          <>
-            <Col span={24} md={12} xl={8}>
-              <Card>
-                <Row justify='space-between'>
-                  <Col span={6}>
-                    <ImagesBox src={NewGroupImage} alt='New Group' />
-                  </Col>
-                  <Col span={12}>
-                    <Button
-                      block
-                      type='primary'
-                      onClick={() => {
-                        navigate('/add-group')
-                      }}
-                      className='add-group-button'
-                    >
-                      Add New Group
-                    </Button>
-                    <Typography.Paragraph style={colorTextTertiaryStyles}>
-                      Add group, if it does not exist
-                    </Typography.Paragraph>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-
-            {groups.map((group) => (
-              <Col key={group?._id} span={24} md={12} xl={8}>
-                <UserGroupCard
-                  name={group?.name}
-                  users={group?.users}
-                  handleEdit={() => handleEdit(group?.name)}
-                />
+        <Col span={24} md={12} xl={8}>
+          <Card>
+            <Row justify='space-between'>
+              <Col span={6}>
+                <ImagesBox src={NewGroupImage} alt='New Group' />
               </Col>
-            ))}
-          </>
-        )}
+              <Col span={12}>
+                <Button
+                  block
+                  type='primary'
+                  onClick={() => {
+                    navigate('/add-group')
+                  }}
+                  className='add-group-button'
+                >
+                  Add New Group
+                </Button>
+                <Typography.Paragraph style={colorTextTertiaryStyles}>
+                  Add group, if it does not exist
+                </Typography.Paragraph>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+
+        {groups.map((group) => (
+          <Col key={group?._id} span={24} md={12} xl={8}>
+            <UserGroupCard
+              name={group?.name}
+              users={group?.users}
+              handleEdit={() => handleEdit(group?.name)}
+            />
+          </Col>
+        ))}
       </Row>
 
       <UserTable />
