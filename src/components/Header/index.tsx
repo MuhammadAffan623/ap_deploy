07d23a80d2ec0d92af0bd0bb6df8904ddf62d02a
Murad khan <murad.jaddoon@gmail.com>
2024-03-21 00:22:27 +0500

    minor fixes in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import { Col, Grid, Layout, Row } from 'antd'
-import React, { CSSProperties, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 import Button from '../Button'
-import SearchField from '../Inputs/SearchField'
+// import SearchField from '../Inputs/SearchField'
 import menuIcon from '~/assets/icons/menu.svg'
-import bellIcon from '~/assets/icons/bell.svg'
+// import bellIcon from '~/assets/icons/bell.svg'
 import './style.scss'
 import UserPopup from '../UserPopup'
 import { useSelector } from 'react-redux'
@@ -16,31 +16,52 @@ interface HeaderProps {
   style?: React.CSSProperties
 }
 
-const searchFieldStyles: CSSProperties = { height: 48 }
+// const searchFieldStyles: CSSProperties = { height: 48 }
 
 const Header = ({ toggleSidebar, style }: HeaderProps) => {
   const { user } = useSelector((state: RootState) => state.user)
-  const [search, setSearch] = useState('')
+  // const [search, setSearch] = useState('')
   const { useBreakpoint } = Grid
   const { sm } = useBreakpoint()
   const [isSearchVisible, setIsSearchVisible] = useState(false)
+  // const [isScrolled, setIsScrolled] = useState(false)
 
-  const navigate = useNavigate()
-  const styleFormItem: CSSProperties = isSearchVisible ? { width: '100%' } : { width: 45 }
+  // const navigate = useNavigate()
+  // const styleFormItem: CSSProperties = isSearchVisible ? { width: '100%' } : { width: 45 }
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value)
-  }
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      console.log(search, 'search')
-    }, 1000)
-    return () => clearTimeout(timeout)
-  }, [search])
+  // const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearch(e.target.value)
+  // }
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     console.log(search, 'search')
+  //   }, 1000)
+  //   return () => clearTimeout(timeout)
+  // }, [search])
 
   useEffect(() => {
     setIsSearchVisible(sm as boolean)
   }, [sm])
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollTop = window.scrollY
+  //     if (scrollTop > 0) {
+  //       setIsScrolled(true)
+  //     } else {
+  //       setIsScrolled(false)
+  //     }
+  //   }
+
+  //   window.addEventListener('scroll', handleScroll)
+
+  //   // Clean up the event listener on unmount
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll)
+  //   }
+  // }, [])
+
+  // console.log('isScrolled', isScrolled)
 
   return (
     <Layout.Header className='userHeader' style={{ ...style }}>
@@ -51,7 +72,7 @@ const Header = ({ toggleSidebar, style }: HeaderProps) => {
               <img src={menuIcon} alt='' />
             </Button>
 
-            <SearchField
+            {/* <SearchField
               inverseBg
               inputClass={['searchField', sm && 'sm'].join(' ')}
               value={search}
@@ -65,20 +86,20 @@ const Header = ({ toggleSidebar, style }: HeaderProps) => {
               onBlur={() => {
                 !sm && setIsSearchVisible(false)
               }}
-            />
+            /> */}
           </div>
         </Col>
 
         <Col span={isSearchVisible ? 0 : 14} sm={12} md={9} lg={6}>
           <div className='userDetailsWrapper'>
             <div className='settings'>
-              <Button
+              {/* <Button
                 type='text'
                 className='toggleSettingsBtn'
                 onClick={() => navigate('/admin/my-account')}
               >
                 <img src={bellIcon} alt='' width={22} />
-              </Button>
+              </Button> */}
             </div>
             <div className='userContainer'>
               <UserPopup user={user} sm={sm} />
