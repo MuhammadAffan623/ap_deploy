32397f89e3cec932f0eb3f0fdef150f94d0cd9bb
bilal-abubaker <bilal_abubeker@hotmail.com>
2024-04-21 23:35:19 +0500

    sidebar schedule dropdown fixed ticket 92

--- src/store/services/_utils.ts
+++ src/store/services/_utils.ts
@@ -1,9 +1,9 @@
-// export const baseUrl =
-//   import.meta.env.VITE_APPLICATION_STATUS === 'development'
-//     ? import.meta.env.VITE_DEVELOPMENT_BASE_URL
-//     : import.meta.env.VITE_PRODUCTION_BASE_URL
+export const baseUrl =
+  import.meta.env.VITE_APPLICATION_STATUS === 'development'
+    ? import.meta.env.VITE_DEVELOPMENT_BASE_URL
+    : import.meta.env.VITE_PRODUCTION_BASE_URL
 
-export const baseUrl = 'https://apsone.atlanticpipe.us/api/v1'
+// export const baseUrl = 'https://apsone.atlanticpipe.us/api/v1'
 
 export const prepareHeaders = (headers: Headers) => {
   const token = localStorage.getItem('token') ?? ''
